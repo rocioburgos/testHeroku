32312a4200884ddbcf145671a8f717a2e3279590
Rocio Burgos <rocioburgos00@gmail.com>
2020-09-03 12:16:53 -0300

    ejercicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ɵConsole    } from '@angular/core';
+import { unsupported } from '@angular/compiler/src/render3/view/util';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pruebaHeroku';
+export class AppComponent    {
+  titulo:string = 'Ciclo de vida';
+   promedio:number =0;
+   suma :number =0;
+   uno;
+   dos;
+  constructor(){}
+   
+   Limpiar(){
+     this.uno='';
+     this.dos='';
+   }
+
+  Calcular( uno:number, dos:number ){
+    
+    uno = parseInt(''+uno);
+    dos = parseInt(''+dos);
+
+    this.suma = uno + dos;
+    this.promedio = (uno + dos )/2;
+
+    //console.log(uno *2);
+    console.log(typeof(parseInt( ''+dos) ) , typeof(uno), uno);
+    console.log(typeof(parseInt( ''+dos) ) , typeof(dos), dos);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BienvenidoComponent } from './componentes/bienvenido/bienvenido.component';
-import { LoginComponent } from './componentes/login/login.component';
-import { ErrorComponent } from './componentes/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BienvenidoComponent,
-    LoginComponent,
-    ErrorComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
