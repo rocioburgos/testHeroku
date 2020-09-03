00ecf9e52869b03456e4588a911ae3af9a528cf4
Rocio Burgos <rocioburgos00@gmail.com>
2020-09-03 10:34:37 -0300

    probando

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/angularapp'));
+app.use(express.static(__dirname + '/pruebaHeroku'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) {
-res.sendFile(path.join(__dirname + '/angularapp/index.html'));
+res.sendFile(path.join(__dirname + '/pruebaHeroku/index.html'));
 });
 console.log('Build successful!!');
+
+
